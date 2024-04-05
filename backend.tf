dd9a195b831a65bcb4023710ca1146fd4b99f77a
GauravBluepi123 <gauravkumar.pandey@bluepi.in>
2024-04-05 16:38:57 +0530

    tf

--- backend.tf
+++ backend.tf
@@ -10,4 +10,8 @@ terraform {
 
 provider "azurerm" {
   features {}
+  subscription_id = f400423d-4718-4f96-9e32-4b3989422b16
+  client_id       = d5a511b9-e410-4ba3-91f1-c454ae29aea1
+  client_secret   = c393c9ec-63fd-4335-b26f-d14fe75ebda6
+  tenant_id       = 41ce6cb6-896d-4960-9d5b-54466d916c1c
 }
