f9872ae7b5bfd829e315625eb888832689be493d
GauravBluepi123 <gauravkumar.pandey@bluepi.in>
2024-04-05 10:56:11 +0530

    tf

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
 terraform {
   cloud {
-    organization = "InterGlobeAviation-Indigo"
+    organization = "BluePi"
 
     workspaces {
-      name = env.TF_WORKSPACE
+      name = "Bluepi-TF-Shared"
     }
   }
 }
