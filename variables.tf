de4f5ba9eb2c2a94483ce4c11b2f823824d3f1bc
GauravBluepi123 <101235561+GauravBluepi123@users.noreply.github.com>
2024-04-05 14:20:01 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -7,15 +7,3 @@ variable "rgname" {
 variable "location" {
   type = string
 }
-variable "ARM_CLIENT_ID" {
-  type = string
-}
-variable "ARM_CLIENT_SECRET" {
-  type = string
-}
-variable "ARM_SUBSCRIPTION_ID" {
-  type = string
-}
-variable "ARM_TENANT_ID" {
-  type = string
-}
