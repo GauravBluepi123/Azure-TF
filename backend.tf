cbe91c304f86bd433ba55c03b4015d68d8f510e4
GauravBluepi123 <101235561+GauravBluepi123@users.noreply.github.com>
2024-04-07 16:38:59 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -11,7 +11,7 @@ terraform {
 provider "azurerm" {
   features {}
   subscription_id = "f400423d-4718-4f96-9e32-4b3989422b16"
-  client_id       = "d5a511b9-e410-4ba3-91f1-c454ae29aea1"
+  client_id       = "iwg8Q~5SZe~hpj.BQRIPNRKZpxb6UdvC-qwSCc~r"
   client_secret   = "c393c9ec-63fd-4335-b26f-d14fe75ebda6"
   tenant_id       = "41ce6cb6-896d-4960-9d5b-54466d916c1c"
 }
