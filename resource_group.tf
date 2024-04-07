f7c6fe48a31da412b997df2efca20f00a933243f
GauravBluepi123 <101235561+GauravBluepi123@users.noreply.github.com>
2024-04-07 16:31:24 +0530

    Update resource_group.tf

--- resource_group.tf
+++ resource_group.tf
@@ -1,6 +1,6 @@
-module "azure_resourcegroup" {
-  source   = "app.terraform.io/BluePi/azure_resourcegroup/azurerm"
-  version  = "1.0.0"
+module "resource-group" {
+  source  = "app.terraform.io/BluePi/resource-group/azurerm"
+  version = "1.0.0"
 
   rgname     = var.rgname
   location   = var.location
