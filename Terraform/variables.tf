8eeb5b4c6322eacd449b21a5c98bc8a268b1a516
GauravBluepi123 <gauravkumar.pandey@bluepi.in>
2024-04-09 16:21:56 +0530

    z

--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -7,9 +7,3 @@ variable "rgname" {
 variable "location" {
   type = string
 }
-variable "subnet_ip" {
-    type = number
-}
-variable "virtual_network_ip" {
-    type = number
-}
--- Terraform/virtual_machine.tf
+++ Terraform/virtual_machine.tf
@@ -1,15 +1,14 @@
 module "azure_vm" {
   source  = "app.terraform.io/BluePi/azure_vm/azurerm"
-  version = "1.0.3"
+  version = "1.0.4"
   
-  ip-addrs_virtual_network = var.virtual_network_ip
+ 
   location = data.azurerm_location.current.display_name
   network_interface_ip_configuration = "my-ip-config"
   network_interface_name = "my-network-interface"
   os_disk_name = "my-os-disk"
   public_ip_name = "my-public-ip"
   rgname = var.rgname
-  subnet_ip_prefix = var.subnet_ip
   subnet_name = "my-subnet"
   virtual_network_name = "my-virtual-network"
   vm_name = "my_vm"
