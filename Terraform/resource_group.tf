be0fd9d21be44fa4ba807411f33be8b030f06052
GauravBluepi123 <gauravkumar.pandey@bluepi.in>
2024-04-09 16:32:24 +0530

    z

--- Terraform/resource_group.tf
+++ Terraform/resource_group.tf
@@ -1,7 +1,7 @@
 module "azure_rg" {
   source  = "app.terraform.io/BluePi/azure_rg/azurerm"
-  version = "1.0.2"
+  version = "1.0.3"
   
   location = var.location
-  rgname = var.rgname
+  rgname = var.rg_name
 }
