ebcc941c758ee73c288bc1a13d65837d98419dc2
GauravBluepi123 <gauravkumar.pandey@bluepi.in>
2024-04-10 11:38:10 +0530

    x

--- Terraform/ag.tf
+++ Terraform/ag.tf
@@ -1,10 +1,10 @@
-module "azure_ag" {
-  source  = "app.terraform.io/BluePi/azure_ag/azurerm"
-  version = "1.0.2"
-  
-  action_group_name = "ActionDemo"
-  ag_short_name = "action"
-  location = data.azurerm_location.current.display_name
-  rgname = module.azure_rg.rgname
-  user_mail_id = "gauravkumar.pandey@bluepi.in"
-}
+#module "azure_ag" {
+#  source  = "app.terraform.io/BluePi/azure_ag/azurerm"
+#  version = "1.0.2"
+#  
+#  action_group_name = "ActionDemo"
+#  ag_short_name = "action"
+#  location = data.azurerm_location.current.display_name
+#  rgname = module.azure_rg.rgname
+#  user_mail_id = "gauravkumar.pandey@bluepi.in"
+#}
--- Terraform/vm_alerts.tf
+++ Terraform/vm_alerts.tf
@@ -1,32 +1,32 @@
-data "azurerm_virtual_machine" "this" {
-  name = module.azure_vm.virtual_name
-  resource_group_name = module.azure_rg.rgname
-}
-
-resource "azurerm_monitor_metric_alert" "this" {
-  for_each = { for idx, threshold in var.cpu_thresholds : idx => threshold }
-
-  name                 = "vm-cpu-usage-alert-${each.value}"
+#data "azurerm_virtual_machine" "this" {
+#  name = module.azure_vm.virtual_name
+#  resource_group_name = module.azure_rg.rgname
+#}
+#
+#resource "azurerm_monitor_metric_alert" "this" {
+#  for_each = { for idx, threshold in var.cpu_thresholds : idx => threshold }
+#
+#  name                 = "vm-cpu-usage-alert-${each.value}"
 #  name                = "vm-cpu-usage-alert"
-  resource_group_name  = module.azure_rg.rgname
-  scopes               = [data.azurerm_virtual_machine.this.id]
-  severity             = "3"
-  criteria {
-    metric_namespace = "microsoft.compute/virtualmachines"
-    metric_name      = "Percentage CPU"
-    aggregation      = "Average"
-    operator         = "GreaterThan"
-    threshold        = each.value
-  }
-
-  action {
-    action_group_id = module.azure_ag.azurerm_monitor_action_group
-  }
-
-  description = "Alert triggered when VM CPU usage exceeds 90%"
-  window_size = "PT5M"
-}
-
+#  resource_group_name  = module.azure_rg.rgname
+#  scopes               = [data.azurerm_virtual_machine.this.id]
+#  severity             = "3"
+#  criteria {
+#    metric_namespace = "microsoft.compute/virtualmachines"
+#    metric_name      = "Percentage CPU"
+#    aggregation      = "Average"
+#    operator         = "GreaterThan"
+#    threshold        = each.value
+#  }
+#
+#  action {
+#    action_group_id = module.azure_ag.azurerm_monitor_action_group
+#  }
+#
+#  description = "Alert triggered when VM CPU usage exceeds 90%"
+#  window_size = "PT5M"
+#}
+#
 #resource "azurerm_monitor_metric_alert" "vm_memory_alert" {
 #  name                = "vm-memory-usage-alert"
 #  resource_group_name = module.azure_rg.rgname
