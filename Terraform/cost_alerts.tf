e6044bdeb635c7cdab47928591f4b4aad319256f
GauravBluepi123 <gauravkumar.pandey@bluepi.in>
2024-04-11 11:36:51 +0530

    a

--- Terraform/cost_alerts.tf
+++ Terraform/cost_alerts.tf
@@ -28,22 +28,22 @@ resource "azurerm_consumption_budget_resource_group" "monthly_budgets" {
   }
 
   # Budget for the upcoming year
-  name_upcoming           = "monthly-budget-${each.value}-${var.current_year + 1}"
-  amount_upcoming         = var.budget_amount
-  time_grain_upcoming     = "Monthly"
-  time_period_end_upcoming = timestamp(date(var.current_year + 1, idx + 1, 1))
+  #name           = "monthly-budget-${each.value}-${var.current_year + 1}"
+  #amount         = var.budget_amount
+  #time_grain     = "Monthly"
+  #time_period_end = timestamp(date(var.current_year + 1, idx + 1, 1))
 
-  category {
-    amount = var.budget_amount
-    category_type = "Cost"
-  }
+  #category {
+  #  amount = var.budget_amount
+  #  category_type = "Cost"
+  #}
 
-  notifications {
-    enabled         = true
-    contact_emails  = ["gauravkumar.pandey@bluepi.in"]
-    contact_roles   = ["Owner"]
-    threshold       = 80
-    operator        = "GreaterThan"
-    notification_key = "monthly-budget-${each.value}-${var.current_year + 1}-80-percent-spent"
-  }
+  #notifications {
+  #  enabled         = true
+  #  contact_emails  = ["gauravkumar.pandey@bluepi.in"]
+  #  contact_roles   = ["Owner"]
+  #  threshold       = 80
+  #  operator        = "GreaterThan"
+  #  notification_key = "monthly-budget-${each.value}-${var.current_year + 1}-80-percent-spent"
+  #}
 }
