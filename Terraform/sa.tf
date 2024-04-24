f4d17ae5602a08b41a33d4049d067b4406cc6933
GauravBluepi123 <gauravkumar.pandey@bluepi.in>
2024-04-24 15:12:27 +0530

    dd

--- Terraform/sa.tf
+++ Terraform/sa.tf
@@ -1,6 +1,6 @@
 module "azure_sa" {
   source  = "app.terraform.io/BluePi/azure_sa/azurerm"
-  version = "1.0.4"
+  version = "1.0.5"
   
   location     = data.azurerm_location.current.display_name
   rgname       = module.azure_rg.rgname
