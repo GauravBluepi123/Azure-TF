0ffd8ace19252c62b47f7f4b6b6a35666cbcb751
GauravBluepi123 <gauravkumar.pandey@bluepi.in>
2024-04-24 15:08:24 +0530

    dd

---
+++ Terraform/event_grid.tf
@@ -0,0 +1,23 @@
+resource "azurerm_storage_queue" "example" {
+  name                 = "examplestoragequeue"
+  storage_account_name = module.azure_sa.sa_name
+}
+
+resource "azurerm_eventgrid_system_topic" "example" {
+  name                   = "example-system-topic"
+  location               = "Global"
+  resource_group_name    = module.azure_rg.rgname
+  source_arm_resource_id = module.azure_rg.rgname_id
+  topic_type             = "Microsoft.Resources.ResourceGroups"
+}
+
+resource "azurerm_eventgrid_system_topic_event_subscription" "example" {
+  name                = "example-event-subscription"
+  system_topic        = azurerm_eventgrid_system_topic.example.name
+  resource_group_name = module.azure_rg.rgname
+
+  storage_queue_endpoint {
+    storage_account_id = module.azure_sa.sa_id
+    queue_name         = azurerm_storage_queue.example.name
+  }
+}
--- Terraform/sa.tf
+++ Terraform/sa.tf
@@ -1,6 +1,6 @@
 module "azure_sa" {
   source  = "app.terraform.io/BluePi/azure_sa/azurerm"
-  version = "1.0.3"
+  version = "1.0.4"
   
   location     = data.azurerm_location.current.display_name
   rgname       = module.azure_rg.rgname
